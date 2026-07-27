c5eb0191ad6f5a3602c1627a221101f5b2053f8e
devgyaraki <rolandgyaraki@gmail.com>
2026-07-27 20:41:42 +0200

    Refactor: improve code readability and add command macros

--- Core/Inc/W25Q128_helper.h
+++ Core/Inc/W25Q128_helper.h
@@ -8,6 +8,45 @@
 #ifndef INC_W25Q128_HELPER_H_
 #define INC_W25Q128_HELPER_H_
 
+#define W25_CMD_WRITE_ENABLE 0x06
+#define W25_CMD_READ_STATUS  0x05
+#define W25_CMD_READ         0x03
+#define W25_CMD_PROGRAM      0x02
+#define W25_CMD_SECTOR_ERASE 0x20
+#define W25_CMD_BLOCK_ERASE  0xD8
+#define W25_CMD_CHIP_ERASE   0xC7
+#define W25_CMD_JEDEC        0x9F
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
+class W25Q128 {
+private:
+    SPI_HandleTypeDef* hspi;
+    GPIO_TypeDef* cs_port;
+    uint16_t cs_pin;
+
+public:
+    W25Q128(SPI_HandleTypeDef* spi, GPIO_TypeDef* port, uint16_t pin);
+    void cs_h(void);
+    void cs_l(void);
+    void write_enable(void);
+    void write(uint32_t addr, uint8_t* buf, uint16_t len);
+    void read(uint32_t addr, uint8_t* pData, uint16_t size);
+    void waitBusy(uint8_t* pData);
+    void Sector_erase(uint32_t addr);
+    void Block_erase(uint32_t addr);
+    void Chip_erase(void);
+    void JEDEC_ID(uint8_t* pData);
+};
+#endif
 
 #endif /* INC_W25Q128_HELPER_H_ */
--- Core/Src/W25Q128_helper.cpp
+++ Core/Src/W25Q128_helper.cpp
@@ -8,49 +8,52 @@
 #include "W25Q128_helper.h"
 #include "main.h"
 
-class W25Q128 {
-	private:
-	SPI_HandleTypeDef* hspi;
-	GPIO_TypeDef* cs_port;
-	uint16_t cs_pin;
-
-	public:
-	W25Q128 (SPI_HandleTypeDef* spi, GPIO_TypeDef* port, uint16_t pin);
-	void write_enable ();
-	void write ();
-	void read ();
-	void erase ();
-	void waitBusy ();
-};
-
 //Konstruktor
 W25Q128 :: W25Q128 (SPI_HandleTypeDef* spi, GPIO_TypeDef* port, uint16_t pin) {
 	hspi = spi;
 	cs_port = port;
 	cs_pin = pin;
-}	//-------------------------------------------Ready
+}
 
-void cs_h (void){
+//Chip select driving high
+void W25Q128 :: cs_h (void){
 	HAL_GPIO_WritePin(cs_port,cs_pin,GPIO_PIN_SET);
-}	//-------------------------------------------Ready
+}
 
-void cs_l (void){
+//Chip select driving low
+void W25Q128 :: cs_l (void){
 	HAL_GPIO_WritePin(cs_port,cs_pin,GPIO_PIN_RESET);
-}	//-------------------------------------------Ready
+}
 
-void W25Q128 :: write_enable () {
-	uint8_t cmd = 0x06;
+
+//Write enable command
+void W25Q128 :: write_enable (void) {
+	uint8_t cmd = W25_CMD_WRITE_ENABLE;
 
 	cs_l ();
 	HAL_SPI_Transmit(hspi, &cmd, 1, 100);
 	cs_h ();
-}	//-------------------------------------------Ready
+}
 
+//Wait until BUSY bit clears
+void W25Q128 :: waitBusy (uint8_t* pData) {
+	uint8_t cmd = W25_CMD_READ_STATUS;
+
+	cs_l ();
+	HAL_SPI_Transmit(hspi, &cmd, 1, 100);
+	HAL_SPI_Receive(hspi, pData, 1, 100);
+	cs_h ();
+}
+
+
+//Page program
 void W25Q128 :: write (uint32_t addr, uint8_t* buf,uint16_t len) {
 	write_enable ();
 
+	uint8_t Status = 0;
+
 	uint8_t cmd [4];
-	cmd [0] = 0x02;
+	cmd [0] = W25_CMD_PROGRAM;
 	cmd [1] = (addr >> 16) & 0xFF;
 	cmd [2] = (addr >> 8) & 0xFF;
 	cmd [3] = addr & 0xFF;
@@ -59,11 +62,15 @@ void W25Q128 :: write (uint32_t addr, uint8_t* buf,uint16_t len) {
 	HAL_SPI_Transmit(hspi, cmd, 4, 100);
 	HAL_SPI_Transmit(hspi, buf, len, 100);
 	cs_h ();
-}	//-------------------------------------------Ready
+
+	do {
+		waitBusy(&Status);
+	}while ((Status & 0x01) == 1);
+}
 
 void W25Q128::read(uint32_t addr, uint8_t* pData, uint16_t size) {
     uint8_t cmd[4];
-    cmd[0] = 0x03;
+    cmd[0] = W25_CMD_READ;
     cmd[1] = (addr >> 16) & 0xFF;
     cmd[2] = (addr >> 8) & 0xFF;
     cmd[3] = addr & 0xFF;
@@ -72,25 +79,16 @@ void W25Q128::read(uint32_t addr, uint8_t* pData, uint16_t size) {
     HAL_SPI_Transmit(hspi, cmd, 4, 100);
     HAL_SPI_Receive(hspi, pData, size, 100);
     cs_h();
-}	//-------------------------------------------Ready
+}
 
-void W25Q128 :: waitBusy (uint8_t* pData) {
-	uint8_t cmd;
-	cmd = 0x05;
-
-	cs_l ();
-	HAL_SPI_Transmit(hspi, &cmd, 1, 100);
-	HAL_SPI_Receive(hspi, pData, 1, 100);
-	cs_h ();
-}	//-------------------------------------------Ready
-
+//Sector deleting
 void W25Q128 :: Sector_erase (uint32_t addr) {
 	write_enable ();
 
 	uint8_t Status = 0;
 
 	uint8_t cmd [4];
-	cmd [0] = 0x20;
+	cmd [0] = W25_CMD_SECTOR_ERASE;
 	cmd [1] = (addr >> 16) & 0xFF;
 	cmd [2] = (addr >> 8) & 0xFF;
 	cmd [3] = addr & 0xFF;
@@ -102,15 +100,16 @@ void W25Q128 :: Sector_erase (uint32_t addr) {
 	do {
 		waitBusy(&Status);
 	}while ((Status & 0x01) == 1);
-}	//-------------------------------------------Ready
+}
 
+//Block deleting
 void W25Q128 :: Block_erase (uint32_t addr) {
 	write_enable ();
 
 	uint8_t Status = 0;
 
 	uint8_t cmd [4];
-		cmd [0] = 0xD8;
+		cmd [0] = W25_CMD_BLOCK_ERASE;
 		cmd [1] = (addr >> 16) & 0xFF;
 		cmd [2] = (addr >> 8) & 0xFF;
 		cmd [3] = addr & 0xFF;
@@ -124,13 +123,14 @@ void W25Q128 :: Block_erase (uint32_t addr) {
 	}while ((Status & 0x01) == 1);
 }
 
-void W25Q128 :: Chip_erase () {
+//Complete Chip deleting
+void W25Q128 :: Chip_erase (void) {
 	write_enable ();
 
 	uint8_t Status = 0;
 
 	uint8_t cmd;
-		cmd = 0xC7;
+	cmd = W25_CMD_CHIP_ERASE;
 
 	cs_l ();
 	HAL_SPI_Transmit(hspi, &cmd, 1, 100);
@@ -139,4 +139,17 @@ void W25Q128 :: Chip_erase () {
 	do {
 		waitBusy(&Status);
 	}while ((Status & 0x01) == 1);
+}
+
+
+//Reas JEDEC ID
+void W25Q128 :: JEDEC_ID (uint8_t* pData) {
+
+	uint8_t cmd;
+	cmd = W25_CMD_JEDEC;
+
+	cs_l ();
+	HAL_SPI_Transmit(hspi, &cmd, 1, 100);
+	HAL_SPI_Receive(hspi, pData, 3, 100);
+	cs_h ();
 }
